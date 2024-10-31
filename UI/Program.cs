4f30490730189694b368e3cad4f9991ae6c14d23
mBoichura2 <132202738+mBoichura2@users.noreply.github.com>
2024-10-31 09:14:50 +0200

    Layered Architecture

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using UI.Controllers;
 using UI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
